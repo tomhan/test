f29677b113b58045b49fbca433208c57848a34f3
rocket <tom@9he.info>
2013-06-09 16:30:25 +0800

    test hello.php

---
+++ info.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: rocket
+ * Date: 13-6-9
+ * Time: 下午4:29
+ * To change this template use File | Settings | File Templates.
+ */
+
+echo  "hello world";
